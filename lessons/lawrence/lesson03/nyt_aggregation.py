cf464f71143375a52e3ddeeca2742fa7742ff45a
Lawrence Qin <lawrence@newbit.io>
2014-02-08 19:02:14 +0800

    Replace calc_ctr function with an inline function

--- lessons/lawrence/lesson03/nyt_aggregation.py
+++ lessons/lawrence/lesson03/nyt_aggregation.py
@@ -7,10 +7,6 @@ for i in range(1, 31):
     csv = pd.read_csv('nyt' + str(i) + '.csv')
     df = df.append(csv)
 
-def calc_ctr(row):
-    ctr = 0.0 if row['Impressions'] == 0 else row['Clicks'] / row['Impressions']
-    return ctr
-
 dfg = df[['Age','Gender','Signed_In', 'Impressions','Clicks']].groupby(['Age','Gender','Signed_In']).sum()
-dfg['CTR'] = dfg.apply(calc_ctr, axis = 1)
+dfg['CTR'] = dfg.apply(lambda row: 0 if['Impressions'] == 0 else row['Clicks'] / row['Impressions'], axis = 1)
 dfg.to_csv('nytimes_aggregation.csv', float_format='%.4f')
