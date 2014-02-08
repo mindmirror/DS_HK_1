bbee6c29fa1309e1945d0d4f23b5bbc09c2d3160
Lawrence Qin <lawrence@newbit.io>
2014-02-08 18:06:55 +0800

    Add nytimes aggregation script

---
+++ lessons/lawrence/lesson03/nyt_aggregation.py
@@ -0,0 +1,16 @@
+from __future__ import division
+import pandas as pd
+import numpy
+
+df = pd.DataFrame()
+for i in range(1, 31):
+    csv = pd.read_csv('nyt' + str(i) + '.csv')
+    df = df.append(csv)
+
+def calc_ctr(row):
+    ctr = 0 if row['Impressions'] == 0 else float(row['Clicks']) / row['Impressions']
+    return ctr
+
+dfg = df[['Age','Gender','Signed_In', 'Impressions','Clicks']].groupby(['Age','Gender','Signed_In']).agg([numpy.sum])
+dfg['CTR'] = dfg.apply(calc_ctr, axis = 1)
+dfg.to_csv('nytimes_aggregation.csv')
